84cda3f3070b22014dea9f092013790d870bc695
SantoshChapagai <santoshchapagai100@gmail.com>
2023-07-22 12:20:58 +0300

    added files

--- app.js
+++ app.js
@@ -140,7 +140,12 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 });
 
-app.listen(3000, function () {
-  console.log("server is running at port 3000")
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("server has started successfully");
 
 });
